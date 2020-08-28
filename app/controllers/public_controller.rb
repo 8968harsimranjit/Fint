973b0c1e5bd0ec38ea65f5283ac4a920209db96e
Harsimranjit Kaur <harsimranchahal170@gmail.com>
2020-08-28 01:33:45 -0700

    added new controller and styles to the application

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -1,5 +1,6 @@
 class PublicController < ApplicationController
 
     def home 
+        @categories = Category.where(parent_id: 1)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_28_061622) do
+ActiveRecord::Schema.define(version: 2020_08_28_081334) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,15 @@ ActiveRecord::Schema.define(version: 2020_08_28_061622) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "parent_id"
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.bigint "category_id"
+    t.string "title"
+    t.text "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["category_id"], name: "index_posts_on_category_id"
   end
 
 end
